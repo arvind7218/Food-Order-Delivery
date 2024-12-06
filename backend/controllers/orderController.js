db588210ce159c55aba7eed26ffea3c4b97e1ad4
arvind7218 <148713365+arvind7218@users.noreply.github.com>
2024-12-07 02:10:50 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://food-express-tlcj.onrender.com";
 
   try {
     // Calculate the total amount of the order from the items
