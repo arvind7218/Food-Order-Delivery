a536b8b65a38a12ada2d1d470163bbe903821eb3
Arvind Chauhan <ac3285960@gmail.com>
2024-12-07 22:07:31 +0530

    auto refresh

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Navigate, Routes, Route } from 'react-router-dom'; // Added Navigate
 import Navbar from './components/Navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
-import { Routes, Route } from 'react-router-dom';
 import Add from './pages/Add/Add';
 import List from './pages/List/List';
 import Orders from './pages/Orders/Orders';
@@ -9,26 +9,29 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-
-  const url = "http://localhost:4000"
+  const url = "http://localhost:4000";
 
   return (
     <div>
-      <ToastContainer/>
+      <ToastContainer />
       <Navbar />
       <hr />
       <div className="app-content">
-        <Sidebar /> {/* Left side - Sidebar */}
-        <div className="main-content"> {/* Right side - Main Content */}
+        <Sidebar />
+        <div className="main-content">
           <Routes>
-            <Route path='/add' element={<Add url={url} />} />
-            <Route path='/list' element={<List url={url} />} />
-            <Route path='/orders' element={<Orders url={url} />} />
+            {/* Default Route */}
+            <Route path="/" element={<Navigate to="/add" />} />
+            
+            {/* Other Routes */}
+            <Route path="/add" element={<Add url={url} />} />
+            <Route path="/list" element={<List url={url} />} />
+            <Route path="/orders" element={<Orders url={url} />} />
           </Routes>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
