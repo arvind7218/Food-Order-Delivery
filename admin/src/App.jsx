50b07b18d35fcdb6326e87ee82a69f85367fb249
arvind7218 <148713365+arvind7218@users.noreply.github.com>
2024-12-08 00:46:48 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,8 +9,8 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url = "http://localhost:4000";
-
+  const url = "https://food-express-backend1.onrender.com";
+// const url = "http://localhost:4000";
   return (
     <div>
       <ToastContainer />
